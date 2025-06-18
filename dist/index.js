ab6b842d66d083ba38feb79c28443cb64ab05cc9
Walter Jansma <walter.jansma@gmail.com>
2025-06-18 16:57:50 +0200

    fix: remove nm and dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-const input = process.argv.slice(2).join(" ");
-if (!input) {
-    console.log("Please enter a git-related command sentence.");
-    process.exit(1);
-}
-console.log(`You said: "${input}"`);
