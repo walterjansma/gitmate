6498cc6c6674b8c631e5f7e122c6bc6b4c82afe0
janvandorth <janvdorth@gmail.com>
2025-06-19 20:14:24 +0200

    feat: add api key logic and add openai call

---
+++ src/apiCallHelper.ts
@@ -0,0 +1,41 @@
+import { getOpenAIApiKey } from "./apiKeyHelper.js"
+
+const url = 'https://api.openai.com/v1/chat/completions';
+
+const body = {
+  model: 'gpt-3.5-turbo',
+  messages: [
+    { role: 'user', content: 'Hello, ChatGPT! How are you today?' }
+  ]
+};
+
+export async function callOpenAI(input: String) {
+
+    const apiKey = await getOpenAIApiKey();
+
+    const body = {
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user', content: input }
+        ]
+    };
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (!response.ok) {
+        console.error(`Error: ${response.status} ${response.statusText}`);
+        const errorText = await response.text();
+        console.error(errorText);
+        return;
+    }
+
+    const data = await response.json();
+    return data;
+}
