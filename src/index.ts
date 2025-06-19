2571e5e85467a4efe67dce14e62218b482c2e33a
janvandorth <janvdorth@gmail.com>
2025-06-19 20:19:17 +0200

    fix: remove comment

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ if (!input) {
 console.log(`You said: "${input}"`);
 console.log(`Processing...`)
 
-// TODO call OpenAI via curl with the provided apiKey and pass the input variable as the input and return the output to the user.
 const response = await callOpenAI(input);
 
 console.log(`OpenAI replies: "${response.choices[0].message.content}"`)
